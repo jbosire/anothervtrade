72e16a57f62f145f5b520c3c38c0393beca17a45
Joram Bosire <jbosire@vassar.edu>
2023-09-03 22:25:59 -0400

    password

--- Backend/config.js
+++ Backend/config.js
@@ -8,10 +8,12 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret_dev";
 const IS_TESTING = process.env.NODE_ENV === "test";
 
 function getDatabaseUri() {
+
   const dbUser = process.env.DATABASE_USER || "postgres";
-  const dbPass = process.env.DATABASE_PASS
-    ? encodeURI(process.env.DATABASE_PASS)
-    : "postgres";
+  const dbPass = encodeURI("Keringet.10")
+  // process.env.DATABASE_PASS
+  //   ? encodeURI(process.env.DATABASE_PASS)
+  //   : "postgres";
   const dbHost = process.env.DATABASE_HOST || "localhost";
   const dbPort = process.env.DATABASE_PORT || 5432;
   const dbProdName = process.env.DATABASE_NAME || "vtrade";
